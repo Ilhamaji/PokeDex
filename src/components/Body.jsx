91389637bfd95cd331c22b74e5d768c7528e0101
ilhamaji <iheadskull@gmail.com>
2024-04-17 16:20:31 +0700

    Second Push

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,10 +5,15 @@ export default function Body() {
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/`).then((res) => {
-      let arr = [res.data.results];
-      setPokemons(...arr);
-    });
+    async function Api() {
+      await axios
+        .get(`https://pokeapi.co/api/v2/pokemon?offset=1000&limit=99999`)
+        .then((res) => {
+          let arr = [res.data.results];
+          setPokemons(...arr);
+        });
+    }
+    Api();
   }, []);
 
   return (
@@ -19,8 +24,9 @@ export default function Body() {
             return (
               <div
                 key={pokemon.name}
-                className="card w-20 h-20 mx-auto bg-slate-200"
+                className="card w-full h-20 mx-auto bg-slate-200"
               >
+                {/* {pokemon.sprites.front_default} */}
                 {pokemon.name}
               </div>
             );
